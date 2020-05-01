d6645e245098839e97ea10146d27af74d9904800
André (gpu-pc) <afcarvalho.1991@gmail.com>
2020-05-01 01:02:40 +0100

    added routing and created app-component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent }    from './home/home.component';
+import { SorterComponent }  from './sorter/sorter.component';
 
-const routes: Routes = [];
+
+const routes: Routes = 
+[
+  { path: '',      redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home'  , component: HomeComponent},
+  { path: 'sorter', component: SorterComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
