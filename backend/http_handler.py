10cd1487f90fe01958bd59b7c717acf95dc6d7c0
André (gpu-pc) <afcarvalho.1991@gmail.com>
2020-05-01 15:34:02 +0100

    completed backend routes

--- backend/http_handler.py
+++ backend/http_handler.py
@@ -12,25 +12,20 @@ def generate_list(size:int): return json.dumps(sorter.generateList(size))
 @app.route('/sorting_algos')
 def sorting_algos(): return json.dumps(sorter.listSortingAlgos())
 
-@app.route('/sort/<string:sort_algo>/<list>/<include_states>')
+@app.route('/sort/<string:sort_algo>/')
 def sort(sort_algo:str):
-    language = request.args.get('language') #if key doesn't exist, returns None
+    ''' expects two parameters list:arr[], and include_states:bool '''
+    # handle and parse list
+    lst = json.loads(request.args.get('list')) # Should return error if is not a valid list or does not exist
+    lst = list(map( lambda v : float(v), list(lst))) # using eval instead of float to avoid having to change if I decide to use int or other datatype
 
-    # using eval instead of float to avoid having to change if I decide to use int or other datatype
-    lst = list(map( lambda v : eval(v), list(lst))) 
-    # converts to boolean
-    if include_states=="true":  include_states = True 
-    else:                       include_states = False
+    # handle states - converts to boolean
+    include_states = request.args.get('states')!=None
 
     return json.dumps(sorter.sort(sort_algo, lst, include_states))
-
-# @app.route('/user/<username>')
-# def profile(username):
-#     return '{}\'s profile'.format(escape(username))
-
+    
 with app.test_request_context():
     print(url_for('generate_list',size=10))
     print(url_for('sorting_algos'))
-    print(url_for('sort',"[0.4,0.2,0.1,1]",))
-    print(url_for('sort',"[0.4,0.2,0.1,1]","true"))
-    # print(url_for('profile', username='John Doe'))
+    print(url_for('sort',sort_algo="algo_name"))
+
