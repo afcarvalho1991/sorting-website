f27ca940c2bb76db454cc68277d22ebdae390e90
afcarvalho-mac <afcarvalho.1991@gmail.com>
2021-12-27 00:45:56 +0000

    kubernetes-deploy

--- kubernetes-deploy.sh
+++ kubernetes-deploy.sh
@@ -1,5 +1,10 @@
 export KUBECONFIG=kubeconfig.yaml && \
 
+# # create nodebalancer
+# helm repo add nginx-stable https://helm.nginx.com/stable && \
+# helm repo update && \
+# helm install ingress-nginx nginx-stable/nginx-ingress --set rbac.create=true && \
+
 kubectl create secret generic regcred \
     --from-file=.dockerconfigjson=/home/afcarvalho/.docker/config.json \
     --type=kubernetes.io/dockerconfigjson
@@ -8,11 +13,12 @@ kubectl apply -f backend/kubernetes/deployment.yaml && \
 kubectl apply -f backend/kubernetes/service.yaml && \
 
 kubectl apply -f web/kubernetes/deployment.yaml && \
-kubectl apply -f web/kubernetes/service.yaml && \
+kubectl apply -f web/kubernetes/service.yaml
 
-# create nodebalancer
-kubectl --namespace default get services -o wide -w ingress-nginx-controller && \
+# kubectl --namespace default get services -o wide -w ingress-nginx-controller
+
 # create ingress
 kubectl apply -f ingress-sorting.yaml && \
+
 
 echo "Completed!"
